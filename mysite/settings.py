fd9a9a83767763e6a5e362bfec9cb40093ef426f
Craig Riley <craig@home.com>
2017-12-17 13:58:15 -0700

    updates to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "static"),
@@ -140,7 +140,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #]
 
 # STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 ## Settings for markdownify
 #MARKDOWNIFY_BLEACH = False
 MARKDOWNIFY_WHITELIST_TAGS = [
